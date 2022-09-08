6be1ff16b50c51980171f929be3ddac52c4254af
Naveen Reddy Sama <nsama6043@sdsu.edu>
2022-09-08 13:19:05 -0700

    modified code based on reviewer comments

--- Assignment1/irisdata.py
+++ Assignment1/irisdata.py
@@ -3,6 +3,14 @@ import sys
 import numpy as np
 import pandas as pd
 import plotly.express as px
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 
 def main():
@@ -19,66 +27,24 @@ def main():
     # converting the data to numpy array and getting summary
     x = data_df.iloc[:, 0:4].values
     # summary of the data
-    print(
-        "Sepal Length Summary [",
-        "Mean :",
-        np.mean(x[:, 0]),
-        "Median :",
-        np.median(x[:, 0], axis=None),
-        "Minimum:",
-        np.min(x[:, 0], axis=None),
-        "Maximum :",
-        np.max(x[:, 0], axis=None),
-        "1st quartile:",
-        np.quantile(x[:, 0], 0.25, axis=None),
-        "3rd Quartile",
-        np.quantile(x[:, 0], 0.75, axis=None),
-    )
-    print(
-        "Sepal Width Summary [",
-        "Mean :",
-        np.mean(x[:, 1]),
-        "Median :",
-        np.median(x[:, 1], axis=None),
-        "Minimum:",
-        np.min(x[:, 1], axis=None),
-        "Maximum :",
-        np.max(x[:, 1], axis=None),
-        "1st quartile:",
-        np.quantile(x[:, 1], 0.25, axis=None),
-        "3rd Quartile",
-        np.quantile(x[:, 1], 0.75, axis=None),
-    )
-    print(
-        "Petal Length Summary [",
-        "Mean :",
-        np.mean(x[:, 2]),
-        "Median :",
-        np.median(x[:, 2], axis=None),
-        "Minimum:",
-        np.min(x[:, 2], axis=None),
-        "Maximum :",
-        np.max(x[:, 2], axis=None),
-        "1st quartile:",
-        np.quantile(x[:, 2], 0.25, axis=None),
-        "3rd Quartile",
-        np.quantile(x[:, 2], 0.75, axis=None),
-    )
-    print(
-        "Petal Width Summary [",
-        "Mean :",
-        np.mean(x[:, 3]),
-        "Median :",
-        np.median(x[:, 3], axis=None),
-        "Minimum:",
-        np.min(x[:, 3], axis=None),
-        "Maximum :",
-        np.max(x[:, 3], axis=None),
-        "1st quartile:",
-        np.quantile(x[:, 3], 0.25, axis=None),
-        "3rd Quartile",
-        np.quantile(x[:, 3], 0.75, axis=None),
-    )
+    columns = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width"]
+    for i in range(0, 4, 1):
+        print(
+            columns[i],
+            " Summary [",
+            "Mean :",
+            np.mean(x[:, i]),
+            "Median :",
+            np.median(x[:, i], axis=None),
+            "Minimum:",
+            np.min(x[:, i], axis=None),
+            "Maximum :",
+            np.max(x[:, i], axis=None),
+            "1st quartile:",
+            np.quantile(x[:, i], 0.25, axis=None),
+            "3rd Quartile",
+            np.quantile(x[:, i], 0.75, axis=None),
+        )
     # scatter plots
     # scatter plot of sepal length and sepal width
     fig = px.scatter(
@@ -89,9 +55,9 @@ def main():
         symbol="species",
         title="Scatter plot of Seoal Length & Sepal Width",
     )
-    fig.show()
+    fig.write_html(file="fig.html", include_plotlyjs="cdn")
     # scatter plot of petal length and petal width
-    fig = px.scatter(
+    fig0 = px.scatter(
         data_df,
         x="petal.width",
         y="petal.length",
@@ -99,7 +65,7 @@ def main():
         symbol="species",
         title="Scatter Plot of Petal Length & Petal Width",
     )
-    fig.show()
+    fig0.write_html(file="fig0.html", include_plotlyjs="cdn")
     # Violin and Box Plots
     fig1 = px.violin(
         data_df, y="sepal.length", box=True, title="Violin & Box Plot of Sepal Length"
@@ -113,10 +79,10 @@ def main():
     fig4 = px.violin(
         data_df, y="petal.width", box=True, title="Violin & Box Plot Petal Width"
     )
-    fig1.show()
-    fig2.show()
-    fig3.show()
-    fig4.show()
+    fig1.write_html(file="fig1.html", include_plotlyjs="cdn")
+    fig2.write_html(file="fig2.html", include_plotlyjs="cdn")
+    fig3.write_html(file="fig3.html", include_plotlyjs="cdn")
+    fig4.write_html(file="fig4.html", include_plotlyjs="cdn")
     # Bar Plots
     fig5 = px.bar(
         data_df,
@@ -146,10 +112,10 @@ def main():
         color="species",
         title="Bar Plot of Petal Width",
     )
-    fig5.show()
-    fig6.show()
-    fig7.show()
-    fig8.show()
+    fig5.write_html(file="fig5.html", include_plotlyjs="cdn")
+    fig6.write_html(file="fig6.html", include_plotlyjs="cdn")
+    fig7.write_html(file="fig7.html", include_plotlyjs="cdn")
+    fig8.write_html(file="fig8.html", include_plotlyjs="cdn")
     # Histogram
     fig9 = px.histogram(
         data_df, x="sepal.length", color="species", title="Histogram of Sepal length"
@@ -163,48 +129,33 @@ def main():
     fig12 = px.histogram(
         data_df, x="petal.width", color="species", title="Histogram of Petal width"
     )
-    fig9.show()
-    fig10.show()
-    fig11.show()
-    fig12.show()
+    fig9.write_html(file="fig9.html", include_plotlyjs="cdn")
+    fig10.write_html(file="fig10.html", include_plotlyjs="cdn")
+    fig11.write_html(file="fig11.html", include_plotlyjs="cdn")
+    fig12.write_html(file="fig12.html", include_plotlyjs="cdn")
     # Transforming the data using standard scaler
-    from sklearn.preprocessing import StandardScaler
-
     scaler = StandardScaler()
     x = scaler.fit_transform(x)
     # Encoding the output variable
     species = data_df.iloc[:, 4].values  # converting output variable to numpy array
-    from sklearn.preprocessing import LabelEncoder
-
     encoder = LabelEncoder()
     species = encoder.fit_transform(species)  # Encoding the out put values
     # Splitting the data to test and training sets
-    from sklearn.model_selection import train_test_split
-
     x_train, x_test, y_train, y_test = train_test_split(
         x, species, test_size=0.3, random_state=42
     )
     # Fitting Random forest against the data
-    from sklearn.ensemble import RandomForestClassifier  # importing the classifier
-
     classifier = RandomForestClassifier()
     classifier.fit(x_train, y_train)  # training the model
     y_pred = classifier.predict(x_test)  # predicting the output using test data
     print("Predicted output values")
     print(y_pred)
-    from sklearn.metrics import accuracy_score, confusion_matrix
-
     print("Random forest model Results:")
     print("Confusion Matrix :")
     print(confusion_matrix(y_pred, y_test))
     print("Accuracy Score :")
     print(accuracy_score(y_pred, y_test) * 100)  # calculating the accuracy of the model
     # Fitting different models using a piple line
-    from sklearn.linear_model import LogisticRegression
-    from sklearn.pipeline import Pipeline
-    from sklearn.svm import SVC
-    from sklearn.tree import DecisionTreeClassifier
-
     randomforestpipeline = Pipeline(
         [("scaler", StandardScaler()), ("Classifier", RandomForestClassifier())]
     )
